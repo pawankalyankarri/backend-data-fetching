b73f36080429b984e6aaa0c74cfcec89d976b061
pawankalyan <pawankalyankarri22@gmail.com>
2024-11-19 18:44:10 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./listItem.css";
+import { useEffect, useState } from "react";
+import ListItem from "./ListItem";
+
+const App = () => {
+  let [list, setList] = useState([]);
+  function fetchItem() {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setList(json));
+  }
+  useEffect(() => {
+    fetchItem();
+  }, []);
+  let retriveItem =
+    list.length > 0 ? (
+      list.map((item) => <ListItem key={item.id} {...item} />)
+    ) : (
+      <div className="col-12">
+        <p style={{ textAlign: "center" }}>Loading...</p>
+      </div>
+    );
+
+  return <div className="container mt-5">{retriveItem}</div>;
+};
+export default App;
---
+++ src/ListItem.js
@@ -0,0 +1,13 @@
+import './listItem.css'
+const ListItem = ({image,price,rating,category}) => {
+ 
+  return (
+   <div className="card shadow">
+    <img src={image} width='250px' />
+    <p>{category}</p>
+    <p>${price}   <br></br>  {rating.rate}</p>
+    
+   </div>
+  );
+};
+export default ListItem;
